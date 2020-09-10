6baa94f65c800795e3bdca7b6d76dda3fe81b000
Zic25 <71065827+Zic25@users.noreply.github.com>
2020-09-10 13:26:54 +0530

    Add files via upload

---
+++ Comman.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRE
+{
+        public enum Level
+        {
+        Physical_Product,
+        Book,
+        Membership,
+        Video,
+        PhysicalOrBooks_Commision,
+        other
+        }
+    
+   
+}
